c205aed532db3a4bbdd5a32755264e0aa616ea7c
Acar, Mert <mert.acar@sky.uk>
2020-01-03 15:30:17 +0000

    Updates

---
+++ precache-manifest.66ae528e9fbb78f9b2fbd6f27caf6881.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f88e7af91566ec3ea9c2878e3acdfb64",
+    "url": "/react-example-1/index.html"
+  },
+  {
+    "revision": "384cbfad82c3acef480b",
+    "url": "/react-example-1/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "a668da61845de5b586ab",
+    "url": "/react-example-1/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "384cbfad82c3acef480b",
+    "url": "/react-example-1/static/js/2.4c8b0761.chunk.js"
+  },
+  {
+    "revision": "83030974b6c2b89a9eb5e33f74cf2213",
+    "url": "/react-example-1/static/js/2.4c8b0761.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a668da61845de5b586ab",
+    "url": "/react-example-1/static/js/main.2b3afca7.chunk.js"
+  },
+  {
+    "revision": "a60fe1c235b5ac08ec35",
+    "url": "/react-example-1/static/js/runtime-main.2607d8e7.js"
+  }
+]);
